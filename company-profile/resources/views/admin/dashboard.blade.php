2efdf944435aa4fde16e1efdd679769630323167
josephany <josephany.twee@gmail.com>
2026-06-11 23:22:51 +0700

    Create dashboard.blade.php

---
+++ company-profile/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,8 @@
+<h1>Dashboard Admin</h1>
+
+<ul>
+    <li><a href="/products">Kelola Product</a></li>
+    <li><a href="/services">Kelola Service</a></li>
+    <li><a href="/company-profile">Company Profile</a></li>
+    <li><a href="/company-location">Company Location</a></li>
+</ul>
